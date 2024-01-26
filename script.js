e8f02473bb7902de4da045563f95d09358b633d5
TSIBO <81839976+Tsi-Bo@users.noreply.github.com>
2024-01-26 17:55:57 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// function updateCustomProperties() {
+//   const root = document.documentElement;
+//   if (window.innerWidth <= 1024) {
+//     root.style.setProperty("--fs-m", "16px");
+//     root.style.setProperty("--fs-l", "26px");
+//     root.style.setProperty("--fs-xl", "30px");
+//     root.style.setProperty("--fs-xxl", "39px");
+//     root.style.setProperty("--fs-bold", "16px");
+//     root.style.setProperty("--fs-btn", "15px");
+//   } else {
+//     // Reset to default values if screen width is greater than 1024px
+//     root.style.setProperty("--fs-m", ""); // Reset to default or another value
+//     root.style.setProperty("--fs-l", "");
+//     root.style.setProperty("--fs-xl", "");
+//     root.style.setProperty("--fs-xxl", "");
+//     root.style.setProperty("--fs-bold", "");
+//     root.style.setProperty("--fs-btn", "");
+//   }
+// }
+
+// updateCustomProperties();
