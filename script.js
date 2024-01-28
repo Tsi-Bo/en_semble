38eabca2cf0143ec57dae711936dd0bb06297ac4
TSIBO <81839976+Tsi-Bo@users.noreply.github.com>
2024-01-28 11:54:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,69 @@
-// function updateCustomProperties() {
-//   const root = document.documentElement;
-//   if (window.innerWidth <= 1024) {
-//     root.style.setProperty("--fs-m", "16px");
-//     root.style.setProperty("--fs-l", "26px");
-//     root.style.setProperty("--fs-xl", "30px");
-//     root.style.setProperty("--fs-xxl", "39px");
-//     root.style.setProperty("--fs-bold", "16px");
-//     root.style.setProperty("--fs-btn", "15px");
-//   } else {
-//     // Reset to default values if screen width is greater than 1024px
-//     root.style.setProperty("--fs-m", ""); // Reset to default or another value
-//     root.style.setProperty("--fs-l", "");
-//     root.style.setProperty("--fs-xl", "");
-//     root.style.setProperty("--fs-xxl", "");
-//     root.style.setProperty("--fs-bold", "");
-//     root.style.setProperty("--fs-btn", "");
-//   }
-// }
+"use strict";
+// Selecting elements
+const navbar = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav__link");
 
-// updateCustomProperties();
+// NAVBAR APPEAR/DISAPPEAR
+document.addEventListener("DOMContentLoaded", function () {
+  // Set the scroll position at which you want the navbar to appear
+  const scrollThreshold = 1000;
+
+  // Event listener for the scroll event
+  window.addEventListener("scroll", function () {
+    // Get the current scroll position
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+    // Toggle the visibility of the navbar based on the scroll position
+    if (scrollPosition > scrollThreshold) {
+      navbar.classList.remove("hidden");
+      console.log("hello");
+    } else {
+      navbar.classList.add("hidden");
+    }
+  });
+});
+
+// SMOOTH SCROLL
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+// Get dates
+function getFutureDate(daysToAdd) {
+  const now = new Date();
+  const futureDate = new Date(now);
+  futureDate.setDate(now.getDate() + daysToAdd);
+  return futureDate;
+}
+
+const atelier1Date = getFutureDate(12);
+const atelier2Date = getFutureDate(18);
+const atelier3Date = getFutureDate(29);
+
+const options = {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+  weekday: "long",
+};
+
+const locale = "fr-FR";
+
+const formattedAtelier1 = new Intl.DateTimeFormat(locale, options).format(
+  atelier1Date
+);
+const formattedAtelier2 = new Intl.DateTimeFormat(locale, options).format(
+  atelier2Date
+);
+const formattedAtelier3 = new Intl.DateTimeFormat(locale, options).format(
+  atelier3Date
+);
+
+// Update HTML elements with the formatted dates
+document.getElementById("atelier1").innerText = "Le : " + formattedAtelier1;
+document.getElementById("atelier2").innerText = "Le : " + formattedAtelier2;
+document.getElementById("atelier3").innerText = "Le : " + formattedAtelier3;
